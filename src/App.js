0b904eaedfaabdac18efea8f3ac7cf6e8e1915dc
Admin <gevorgmatem@gmail.com>
2023-08-30 01:53:28 +0400

    adding-a-cart-reducer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment, useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Carts/Cart";
+import CartProvider from "./store/CartProvider";
 
 
 
@@ -17,13 +18,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,17 @@
-import CartIcon from "../Carts/CartIcon";
+import { useContext } from 'react';
 
-import classes from "./HaderCardButton.module.css";
+import CartIcon from "../Carts/CartIcon";
+import CartContext from '../../store/cart-context';
+import classes from './HaderCardButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  // Ensure cartCtx.items is an array before using reduce
+  console.log('cartCtx: ', cartCtx, cartCtx.items)
+  const numberOfCartItems = cartCtx.items
+    ? cartCtx.items.reduce((curNumber, item) => curNumber + item.amount, 0)
+    : 0;
 
   return (
     <button className={classes.button} onClick={props.onClick}>
@@ -10,7 +19,7 @@ const HeaderCartButton = (props) => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
